e151310e2b969a7a86979c8a3d3b31d1231232ea
ItsJustAPlayer <48572780+itsjustaplayer@users.noreply.github.com>
2025-12-31 20:06:31 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -14,15 +14,15 @@ window.location.href = newPath;
 
 // Disable widestore class from fucking up the theme, by ItsJustAPlayer
 
-const targetClass = 'widestore';
-const replacementClass = 'widestoredisabled'; // leave empty to remove completely, or set a custom name if you want
-const body = document.body;
-if (body.classList.contains(targetClass)) {
-    if (replacementClass) {
-        body.classList.replace(targetClass, replacementClass);
-    } 
-    else {
-        body.classList.remove(targetClass);
-    }
-};
+//const targetClass = 'widestore';
+//const replacementClass = 'widestoredisabled'; // leave empty to remove completely, or set a custom name if you want
+//const body = document.body;
+//if (body.classList.contains(targetClass)) {
+   // if (replacementClass) {
+     //   body.classList.replace(targetClass, replacementClass);
+    //} 
+    //else {
+   //     body.classList.remove(targetClass);
+   // }
+//};
    
